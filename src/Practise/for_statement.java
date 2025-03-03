9e5d69e44f1888ecc0c661fac1db634fd279da7a
shanmugam1510 <shangraphics84@gmail.com>
2025-03-03 07:04:44 +0530

    first commit

---
+++ src/Practise/for_statement.java
@@ -0,0 +1,9 @@
+package Practise;
+
+public class for_statement {
+    public static void main(String[] args) {
+        int i = 10;
+        for (int a = i; i <= 10; i = i + 1) ;
+        System.out.println(i);
+    }
+}
